8527efc359bada04b1d790d5afb0a7611efb53c5
unknown <saeed.salami84@gmail.com>
2024-10-20 13:25:41 +0330

    add a test method

--- src/test/java/com/example/testforjenkings/TestforjenkingsApplicationTests.java
+++ src/test/java/com/example/testforjenkings/TestforjenkingsApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.testforjenkings;
 
+import com.example.testforjenkings.controller.HomeController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TestforjenkingsApplicationTests {
 
+    @Autowired
+    HomeController homeController;
+
     @Test
     void contextLoads() {
+
+        assert(homeController).homePage().equals("index");
     }
+
 
 }
